3fe3291417be5e63f3d4db99acce642870b799da
Jagan <psbsvdmvcg@gmail.com>
2025-09-23 00:39:19 +0530

    added user and video models

---
+++ src/models/user.model.js
@@ -0,0 +1,91 @@
+import mongoose,{Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+
+const userSchema = new Schema(
+    {
+        username : {
+            type : String,
+            required : true,
+            unique : true,
+            lowercase : true,
+            trim : true,
+            index : true // since we want to search based on the username 
+        },
+        email : {
+            type : String,
+            required : true,
+            unique : true,
+            lowercase : true,
+            trim : true, 
+        },
+        fullName : {
+            type : String,
+            required : true,
+            trim : true, 
+            index : true
+        },
+        avatar : {
+            type : String,
+            required : true
+        },
+        coverimage : {
+            type : String
+        },
+        watchHistory : [
+            {
+                type : Schema.Types.ObjectId,
+                ref : "Video"
+            }
+        ],
+        password : {
+            type : String,
+            required : [true,'Password is required']
+        },
+        refreshToken : {
+            type : String
+        }
+    },
+    {timestamps : true}
+)
+
+//now we need to hash the password just before saving 
+userSchema.pre("save",async function (next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+            email : this.email,
+            username : this.username,
+            fullName : this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User",userSchema)
+//the db will store it as users ie smaller case and plural 
