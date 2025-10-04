ebf5484b2a61a7b90c2b7e59172b5328590ca31f
Jagan <psbsvdmvcg@gmail.com>
2025-10-04 19:34:16 +0530

    added healthcheck controller

---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,26 @@
+// what does health check means ?? 
+// what is the use of health check controller ?
+// in the assignment it just said it should simply 
+// returns the ok status as json with a message ??
+// if this is what we do in this controller 
+// what happens when we click /healthcheck 
+// this will gives the ok status with some response 
+// what is the use of this ??? 
+// why do we need this controller 
+
+// this is to know whether the server is alive or not 
+// this is the first thing we check when our app isn't 
+// responding 
+
+// now how to write this 
+
+import mongoose from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const healthcheck = asyncHandler( async(req,res)=>{
+    return res
+    .status(200)
+    .json({"status" : "ok","message" : "Server is running"})
+})
+
+export {healthcheck}
